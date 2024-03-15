597dd7602aabd88a4197f855cd88d8c723f4b5c0
Mateus Ferreira <mateusferreira0704@gmail.com>
2024-03-15 09:17:41 -0300

    Changing the main page

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -3,23 +3,42 @@ import styles from "./about.module.scss";
 
 const About = () => {
 	return (
-		<main className={styles.mainAbout}>
+		<section className={styles.mainAbout}>
 			{/* <Image src="/mateus-nobackground.png" width={160} height={160} alt="Mateus" className={styles.profile}/> */}
-            <section>
-                <h1 className={styles.title}>Mateus Ferreira</h1>
-                <h2 className={styles.profession}>Mid Frontend Engineer</h2>
-                <p>I craft digital experiences that are pixel-perfect, captivating, and easily accessible.</p>
-                <ul className={styles.containerLi}>
-                    <li className={styles.link}>Bachelor Degree in Software Engineering</li>
-                    <li className={styles.link}><strong>Over 3 years experience</strong> in Frontend development</li>
-                    <li className={styles.link}>Main stacks: <strong>React and Angular</strong></li>
-                </ul>
-                <a href="/resumeMateus.pdf" target="_blank">
-                    <button className={styles.buttonDownload}>View Full Resume</button>
-                </a>
-            </section>
-            <section></section>            
-		</main>
+			<section className={styles.leftSide}>
+				<h1 className={styles.title}>Mateus Ferreira</h1>
+				<h2 className={styles.profession}>Mid Frontend Engineer</h2>
+				<p>I craft digital experiences that are pixel-perfect, captivating, and easily accessible.</p>
+				<ul className={styles.containerLi}>
+					<li className={styles.link}>Bachelor Degree in Software Engineering</li>
+					<li className={styles.link}>
+						<strong>Over 3 years experience</strong> in Frontend development
+					</li>
+					<li className={styles.link}>
+						Main stacks: <strong>React and Angular</strong>
+					</li>
+				</ul>
+				<a href="/resumeMateus.pdf" target="_blank">
+					<button className={styles.buttonDownload}>View Full Resume</button>
+				</a>
+			</section>
+			<section className={styles.rigthSide}>
+				<p>
+					My interest in technology came to life when my brother brought an Arduino project home. Although it
+					was something simple, consisting only of sensors and LEDs, that experience sparked something in me.
+					It was as if a new dimension had opened up before my eyes, and since then, I couldn't stray from
+					this path anymore.
+				</p>
+				<p>
+					My main focus these days is building accessible user interfaces for our customers at Klaviyo. I most
+					enjoy building software in the sweet spot where design and engineering meet
+				</p>
+				<p>
+					When I’m not at the computer, I’m usually rock climbing, reading, hanging out with my wife and two
+					cats, or running around Hyrule searching for Korok seeds K o r o k s e e d s
+				</p>
+			</section>
+		</section>
 	);
 };
 
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -4,44 +4,46 @@ import styles from "./header.module.scss";
 const Header = () => {
 	return (
 		<header className={styles.header}>
-			<img className={styles.logo} src="/ferreiras-branco.png" alt="logo" />
-			<nav className={styles.rightSide}>
-				<ul className={styles.nav}>
-					<li>
-						<a href="" className={styles.link}>
-							About
-						</a>
-					</li>
-					<li>
-						<a href="" className={styles.link}>
-							Experience
-						</a>
-					</li>
-					<li>
-						<a href="" className={styles.link}>
-							Projects
-						</a>
-					</li>
-					<li>
-						<ul className={styles.nav} style={{ marginLeft: 30 }}>
-							<li>
-								<a
-									href="https://www.linkedin.com/in/mateus-ferreira0704/"
-									target="_blank"
-									className={styles.link}
-								>
-									<Image src="linkedin.svg" alt="Linkedin Icon" width={30} height={30} />
-								</a>
-							</li>
-							<li>
-								<a href="https://github.com/mfladeira" target="_blank" className={styles.link}>
-									<Image src="/github.png" alt="Github Icon" width={30} height={30} />
-								</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</nav>
+			<div className={styles.container}>
+				<img className={styles.logo} src="/ferreiras-branco.png" alt="logo" />
+				<nav className={styles.rightSide}>
+					<ul className={styles.nav}>
+						<li>
+							<a href="" className={styles.link}>
+								About
+							</a>
+						</li>
+						<li>
+							<a href="" className={styles.link}>
+								Experience
+							</a>
+						</li>
+						<li>
+							<a href="" className={styles.link}>
+								Projects
+							</a>
+						</li>
+						<li>
+							<ul className={styles.nav} style={{ marginLeft: 30 }}>
+								<li>
+									<a
+										href="https://www.linkedin.com/in/mateus-ferreira0704/"
+										target="_blank"
+										className={styles.link}
+									>
+										<Image src="linkedin.svg" alt="Linkedin Icon" width={30} height={30} />
+									</a>
+								</li>
+								<li>
+									<a href="https://github.com/mfladeira" target="_blank" className={styles.link}>
+										<Image src="/github.png" alt="Github Icon" width={30} height={30} />
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 };
