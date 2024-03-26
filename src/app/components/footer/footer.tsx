746f76adfb4031cc06ef26ed6149724d2ecf0305
Mateus Ferreira <mateusferreira0704@gmail.com>
2024-03-26 15:28:57 -0300

    Created Footer

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.scss";
+
+const Footer = () => {
+	return (
+		<footer className={styles.Footer}>
+			<p className={styles.text}>Designed & built by Mateus Ferreira</p>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
-
 import Header from "./components/header/header";
 import styles from "./page.module.css";
 import About from "./components/about/about";
 import Experiences from "./components/experiences/experiences";
 import Projects from "./components/projects/projects";
+import Footer from "./components/footer/footer";
 
 export default function Home() {
-	// const displayLight = (event: any) => {
-	//   console.log(event.pageX)
-	//   console.log(event.pageY)
-	// onMouseMove={(event) => displayLight(event)}
-	// }
-
 	return (
 		<main className={styles.main}>
 			<Header />
 			<About />
 			<Experiences />
 			<Projects />
+			<Footer />
 		</main>
 	);
 }
